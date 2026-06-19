2bcd83e23ff8b35bfc914e7058dc90df76de555d
Kevin <kevinferreira2505@gmail.com>
2026-06-19 19:28:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,101 @@
 import os
 
-# Variáveis Globais
-opcao = 0
-Usuários = []
-Livros = []
+
+# Variáveis Globais com dados do Carlos e do Kevin já salvos para o teste
+Usuários = [{"username": "carlos", "nome": "Carlos"}, {"username": "kevin", "nome": "Kevin"}]
+Livros = [{"codigo": "1", "nome": "O Alquimista", "qtd": 1}], [{"codigo": "2", "nome": "Pequeno Principe", "qtd": 2}]
 Empréstimos = []
 
-def clear_screen():
-   os.system('cls' if os.name == 'nt' else 'clear')
-
-def show_menu(menu):
-   if(menu == 'principal'):
-      print('1. Clientes')
-      print('2. Produtos')
-      print('3. Vendas')
-      print('0. Sair')
 
 while True:
-   clear_screen()
-   print('Controle de Estoque')
-   show_menu('principal')
+   os.system('cls' if os.name == 'nt' else 'clear')
+   print('=== CONTROLE DE ESTOQUE ===')
+   print('1. Usuários | 2. Produtos | 3. Empréstimos | 0. Sair')
+   opcao = input('Digite a opção desejada: ')
 
-   opcao = int(input('Digite a opção desejada: '))
 
-   if(opcao == 1):
-      print('Usuarios')
-      print('1. Novo Usuário')
-      print('2. Listar Usuários')
-      print('3. Voltar')
-   elif(opcao == 2):
-      print('Produtos')
-   elif(opcao == 3):
-      print('Vendas')
-   elif(opcao == 0):
+   if opcao == "0": 
+      print("Saindo do sistema... Valeu!")
       break
-   else:
-      print('Opção Invalida...')
+
+
+   # MENU 1: USUÁRIOS
+   elif opcao == "1":
+      print('\n=== MENU: USUÁRIOS ===')  # <- ADICIONADO AQUI
+      print('1. Novo Usuário | 2. Listar | 3. Excluir')
+      acao = input('Escolha a ação: ')
+      if acao == "1":
+            user = input("Username único: ")
+            existe = False
+            for u in Usuários:
+               if u['username'] == user: existe = True
+            
+            if existe: 
+               print("Erro: Username já existe!")
+            else: 
+               Usuários.append({"username": user, "nome": input("Nome: ")})
+               print("Cadastrado!")
+      elif acao == "2":
+            for u in Usuários: print(f" Username: {u['username']} | Nome: {u['nome']}")
+      elif acao == "3":
+            user = input
+            input("Username para remover: ")
+            Usuários = [u for u in Usuários if u['username'] != user]
+
+   # MENU 2: PRODUTOS
+   elif opcao == "2":
+      print('\n=== MENU: PRODUTOS ===')  # <- ADICIONADO AQUI
+      print('1. Novo Produto | 2. Listar | 3. Excluir')
+      acao = input('Escolha a ação: ')
+      if acao == "1":
+            cod = input("Código único: ")
+            existe = False
+            for l in Livros:
+               if l['codigo'] == cod: existe = True
+               
+            if existe: 
+               print("Erro: Código já existe!")
+            else: 
+               Livros.append({"codigo": cod, "nome": input("Nome do Livro: "), "qtd": int(input("Qtd: "))})
+               print("Livro Cadastrado!")
+      elif acao == "2":
+            for l in Livros: print(f" Código: {1['codigo']} | Livro: {l['nome']} | Estoque: {l['qtd']}")
+            for 2 in Livros: print(f" Código: {2['codigo']} | Livro: {2['nome']} | Estoque: {2['qtd']}") Livros = ([{...}]), ([{...}]), 2 = [{'codigo': '2', 'nome': 'Pequeno Principe', 'qtd': 2}]
+      elif acao == "3":
+            cod = input("Código para remover: ")
+            Livros = [l for l in Livros if l['codigo'] != cod]
+
+   # MENU 3: VENDAS (EMPRÉSTIMOS)
+   elif opcao == "3":
+      print('\n=== MENU: VENDAS (EMPRÉSTIMOS) ===')  # <- ADICIONADO AQUI
+      print('1. Novo Empréstimo | 2. Listar | 3. Excluir')
+      acao = input('Escolha a ação: ')
+      if acao == "1":
+            user = input("Username: ")
+            cod = input("Código do Livro: ")
+            
+            livro_encontrado = None
+            for l in Livros:
+               if l['codigo'] == cod:
+                  livro_encontrado = l
+            
+            if livro_encontrado and livro_encontrado['qtd'] > 0:
+               livro_encontrado['qtd'] -= 1  
+               Empréstimos.append({"user": user, "codigo": cod})
+               print("Empréstimo feito!")
+            else: 
+               print("Erro: Sem estoque! Não permitido estoque negativo.")
+      elif acao == "2":
+            for e in Empréstimos: print(f" Usuário: {e['user']} | Livro Código: {e['codigo']}")
+      elif acao == "3":
+            cod = input("Código do livro para devolver: ")
+            for l in Livros:
+               if l['codigo'] == cod: l['qtd'] += 1  
+            Empréstimos = [e for e in Empréstimos if e['codigo'] != cod]
+
+   else: 
+      print('Opção Inválida...')
+      
+   input('\nPressione Enter para continuar...')
+
+
